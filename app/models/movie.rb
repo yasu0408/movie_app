fb730de6d900152add77b42ca7286df40a0a5716
yasu0408 <choppaya.ketsukacchin@gmail.com>
2021-07-05 09:33:03 +0900

    動画の表示のバグを修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,15 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true
+
+  before_save do
+    format_url = YoutubeUrlFormatter.format(url)
+    if format_url.present?
+      self.url = format_url
+    else
+      # 失敗した場合はバリデーションエラーを出す
+      self.errors.add(:url, "YouTubeのURL以外は無効です")
+      throw(:abort)
+    end
+  end
 end
