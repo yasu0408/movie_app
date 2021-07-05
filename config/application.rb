7f15587dbbc9a7c2bef093ea653e1d32e591396d
yasu0408 <choppaya.ketsukacchin@gmail.com>
2021-07-05 09:10:38 +0900

    URLのフォーマット用クラスを作成

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module MovieApp
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = "Asia/Tokyo"
+    config.autoload_paths << Rails.root.join("lib/autoloads")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
